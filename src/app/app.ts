45c6164bf293c3ba18fa7119cd1fa9425ddfd7c9
Ahmed Said Elgizawy <elgizawy574@gmail.com>
2026-03-23 14:24:45 +0200

    init

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,52 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,RouterLink,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('app');
+  // === menu ===
   IsOpened = false
   openMenu(){
     this.IsOpened = !this.IsOpened
+  }
+
+
+
+// === theme ===
+isDark = false;
+
+toggleTheme() {
+  this.isDark = !this.isDark;
+
+  if (this.isDark) {
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+  }
+}
+
+// === scroll-up ===
+  scrollPercent = 0;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollTop = window.scrollY;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = document.documentElement.clientHeight;
+
+    const totalScrollable = scrollHeight - clientHeight;
+
+    this.scrollPercent = (scrollTop / totalScrollable) * 100;
+    console.log(this.scrollPercent);
+    
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
